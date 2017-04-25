6805885a5d46661d488a32bebc80e0f3d795f169
Alif <alifaziz@gmail.com>
2017-04-25 15:19:31 +0800

    Add cover photo modal view

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Text, View, Image, Linking, TouchableOpacity, Modal } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
@@ -7,60 +7,62 @@ import SocialShare from './SocialShare';
 import PropTypes from 'prop-types';
 import ImageViewer from 'react-native-image-zoom-viewer';
 
+class AlbumDetail extends Component {
 
-const _showCoverPhoto = (imgUrl) => (
-    <Modal onRequestClose={() => { }} visible={true} transparent={false}>
-        <ImageViewer imageUrls={['https://avatars2.githubusercontent.com/u/7970947?v=3&s=460']} />
-    </Modal>
-)
+    constructor(props) {
+        super(props);
 
-const AlbumDetail = ({ album }) => {
+        this.state = {
+            showModal: false,
+            modalImageSource: ''
+        }
 
-    const url = 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Dmovies-tv&field-keywords=';
-    const { Title, Year, Poster, image } = album;
-    const {
-        thumbnailStyle,
-        headerContentStyle,
-        thumbnailContainerStyle,
-        headerTextStyle,
-        imageStyle
-  } = styles;
+        this._url = 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Dmovies-tv&field-keywords=';
+    }
 
-    return (
-        <Card>
-            <CardSection>
-                <View style={thumbnailContainerStyle}>
-                    <Image
-                        style={thumbnailStyle}
-                        source={{ uri: Poster }}
-                    />
-                </View>
-                <View style={headerContentStyle}>
-                    <Text numberOfLines={1} ellipsizeMode={'tail'} style={headerTextStyle}>{Title}</Text>
-                    <Text>Year {Year}</Text>
-                </View>
-            </CardSection>
 
-            <TouchableOpacity activeOpacity={0.7} onPress={() => { _showCoverPhoto() }}>
+
+    render() {
+        return (
+            <Card>
+                <Modal animationType={'fade'} onRequestClose={() => { this.setState({ showModal: false }) }} visible={this.state.showModal} transparent={false}>
+                    <ImageViewer imageUrls={[{url: this.state.modalImageSource}]} />
+                </Modal>
                 <CardSection>
-                    <Image
-                        style={imageStyle}
-                        source={{ uri: Poster }}
-                    />
+                    <View style={styles.thumbnailContainerStyle}>
+                        <Image
+                            style={styles.thumbnailStyle}
+                            source={{ uri: this.props.album.Poster }}
+                        />
+                    </View>
+                    <View style={styles.headerContentStyle}>
+                        <Text numberOfLines={1} ellipsizeMode={'tail'} style={styles.headerTextStyle}>{this.props.album.Title}</Text>
+                        <Text>Year {this.props.album.Year}</Text>
+                    </View>
                 </CardSection>
-            </TouchableOpacity>
 
-            <CardSection>
-                <Button onPress={() => Linking.openURL(url + Title)}>
-                    Buy Now
+                <TouchableOpacity activeOpacity={0.7} onPress={() => { this.setState({ modalImageSource: this.props.album.Poster, showModal: true }); }}>
+                    <CardSection>
+                        <Image
+                            style={styles.imageStyle}
+                            source={{ uri: this.props.album.Poster }}
+                        />
+                    </CardSection>
+                </TouchableOpacity>
+
+                <CardSection>
+                    <Button onPress={() => Linking.openURL(this._url + this.props.album.Title)}>
+                        Buy Now
                 </Button>
-                <SocialShare title={'Share to..'} msg={'Checkout this movie ' + url + encodeURIComponent(Title) + ' #reactnative #react'} url={url + encodeURIComponent(Title)} >
-                    Share
+                    <SocialShare title={'Share to..'} msg={'Checkout this movie ' + this._url + encodeURIComponent(this.props.album.Title) + ' #reactnative #react'} url={this._url + encodeURIComponent(this.props.album.Title)} >
+                        Share
                 </SocialShare>
-            </CardSection>
-        </Card>
-    );
-};
+                </CardSection>
+            </Card>
+        )
+    }
+
+}
 
 AlbumDetail.propTypes = {
     album: PropTypes.any.isRequired
